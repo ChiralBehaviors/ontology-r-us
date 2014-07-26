8fb0581fc0ed5173aad004b230bc7cea6a3b3842
Hellblazer <hellblazer@me.com>
2014-07-26 11:06:46 -0700

    Refactor to a multi-module project.  Add the plugin module for generating Java classes from OWL models.

---
+++ owl-java-generation-plugin/src/main/java/com/chiralbehaviors/ultrastructure/plugin/owl/OwlToJavaGenerator.java
@@ -0,0 +1,58 @@
+/** 
+ * (C) Copyright 2012 Chiral Behaviors, LLC. All Rights Reserved
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *     
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package com.chiralbehaviors.ultrastructure.plugin.owl;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author hhildebrand
+ * 
+ */
+/*
+ * Copyright (c) 2009, 2011 Hal Hildebrand, all rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author hhildebrand
+ * 
+ * @goal load
+ * 
+ * @phase package
+ */
+public class OwlToJavaGenerator extends AbstractMojo {
+    /* (non-Javadoc)
+     * @see org.apache.maven.plugin.Mojo#execute()
+     */
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("Loading database");
+    }
+}
